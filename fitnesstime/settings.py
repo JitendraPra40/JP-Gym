bf2ec1cac71ed90da222461d828c430d0774f510
JitendraPra40 <jitendra.pra40@gmail.com>
2025-01-15 13:15:57 +0530

    deploy

--- fitnesstime/settings.py
+++ fitnesstime/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-_tbg7wxo(ae2pgfd)9kqj7bn1oq0hha7l=eu+iyyigyx$9qfpb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,12 +75,12 @@ WSGI_APPLICATION = 'fitnesstime.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
@@ -117,10 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 import os
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR / "static")
+    os.path.join(BASE_DIR,'staticfiles_build',"static")
     ]
 
 MEDIA_URL = '/media/' 
