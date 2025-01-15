878e49e76ce8db5dad611ef1376df304d4b0e6f2
JitendraPra40 <jitendra.pra40@gmail.com>
2025-01-15 13:34:57 +0530

    r

--- fitnesstime/settings.py
+++ fitnesstime/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 import os
 STATIC_ROOT = [
-    os.path.join(BASE_DIR,"static")
+    os.path.join(BASE_DIR, 'staticfiles_build', 'static')
     ]
 
 MEDIA_URL = '/media/' 
